98829f53cf65826aae315cb036877d3f42614206
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-11-19 09:33:18 -0600

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
